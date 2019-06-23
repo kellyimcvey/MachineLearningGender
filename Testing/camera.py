6e57ce737d37a5f3e23067ea646d2323d6d0dab4
JulianYanez <julianf.yanez@gmail.com>
2019-06-23 16:05:33 -0500

    new files and updated camera

--- Testing/camera.py
+++ Testing/camera.py
@@ -1,6 +1,12 @@
+import keras_vggface
+from mtcnn.mtcnn import MTCNN
+import numpy as np
 import cv2
-video = 0
+import matplotlib.pyplot as plt
+from PIL import Image
 
+video = 0
+detector = MTCNN()
 class VideoCamera(object):
     def __init__(self):
     	global video
@@ -12,7 +18,24 @@ class VideoCamera(object):
 
     def get_frame(self):
     	global video
+    	# grabs webcam image
     	ret, frame = self.video.read()
     	# DO WHAT YOU WANT WITH TENSORFLOW / KERAS AND OPENCV
-    	ret, jpeg = cv2.imencode('.jpg', frame)
-    	return jpeg.tobytes()
+    	# print(frame.shape)
+    	# detects faces with mtcnn. If no face detected. Except catches error and returns normal webcam image
+    	results = detector.detect_faces(frame) #errors out if no face
+    	#getting outline of face
+    	x1, y1, width, height = results[0]['box']
+    	x2, y2 = x1 + width, y1 + height
+    	# cropping to face
+    	face = frame[y1:y2, x1:x2]
+    	# creates PIL image object
+    	image = Image.fromarray(face)
+    	image = image.resize((224, 224))
+	    # run face array into keras model
+	    face_array = np.asarray(image)
+	    # MORE HERE
+	    ret, jpeg = cv2.imencode('.jpg', face_array)
+	    return jpeg.tobytes()
+	    # ret, jpeg = cv2.imencode('.jpg', frame)
+	    # return jpeg.tobytes()
