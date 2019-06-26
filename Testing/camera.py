f745d7c3e43ec91f82e7a1aa6527b9012e520933
JulianYanez <julianf.yanez@gmail.com>
2019-06-25 22:41:36 -0500

    activity notebook and data file

--- Testing/camera.py
+++ Testing/camera.py
@@ -23,19 +23,24 @@ class VideoCamera(object):
     	# DO WHAT YOU WANT WITH TENSORFLOW / KERAS AND OPENCV
     	# print(frame.shape)
     	# detects faces with mtcnn. If no face detected. Except catches error and returns normal webcam image
-    	results = detector.detect_faces(frame) #errors out if no face
-    	#getting outline of face
-    	x1, y1, width, height = results[0]['box']
-    	x2, y2 = x1 + width, y1 + height
-    	# cropping to face
-    	face = frame[y1:y2, x1:x2]
-    	# creates PIL image object
-    	image = Image.fromarray(face)
-    	image = image.resize((224, 224))
-	    # run face array into keras model
-	    face_array = np.asarray(image)
-	    # MORE HERE
-	    ret, jpeg = cv2.imencode('.jpg', face_array)
-	    return jpeg.tobytes()
+    	try:
+    		#detect face with MTCNN
+    		results = detector.detect_faces(frame)
+    		#getting outline of face
+    		x1, y1,width, height = results[0]['box']
+    		x2, y2 = x1+width, y1+height
+    		#cropping to face
+    		face = frame[y1:y2, x1:x2]
+    		#create PIL image object
+    		image = Image.fromarray(face)
+    		image = image.resize((224,224))
+    		#run face array into keras model
+    		face_array = np.asarray(image)
+    		#MORE HERE
+    		ret, jpeg = cv2.imencode('.jpg',face_array)
+    		return jpeg.tobytes()
+    	except:
+    		ret, jpeg = cv2.imencode('.jpg',frame)
+    		return jpeg.tobytes()
 	    # ret, jpeg = cv2.imencode('.jpg', frame)
 	    # return jpeg.tobytes()
